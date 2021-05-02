284f116d2c3ea36b3b03327dbac9aa9837941382
Maxime Blanc <max@jellycat.fr>
2021-05-02 21:17:28 +0200

    associated functions

--- chapter_5/rectangles/src/main.rs
+++ chapter_5/rectangles/src/main.rs
@@ -5,21 +5,42 @@ struct Rectangle {
 }
 
 impl Rectangle {
+    fn new(width: u32, height: u32) -> Rectangle {
+        Rectangle { width, height }
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+
     fn area(&self) -> u32 {
         self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
 
 fn main() {
-    let rectangle = Rectangle {
+    let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
-    println!("{:#?}", rectangle);
+    println!("{:#?}", rect1);
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        rectangle.area()
+        rect1.area()
     );
+
+    let rect2 = Rectangle::new(50, 40);
+    let square1 = Rectangle::square(20);
+
+    println!("Do rect1 can hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Do rect1 can hold square1? {}", rect1.can_hold(&square1));
 }
