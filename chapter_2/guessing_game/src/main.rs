87a396e25b4df3f6dcfe1a12700c40d283817a95
Maxime Blanc <max@jellycat.fr>
2021-05-05 23:11:34 +0200

    Chapter 9 (#11)

--- chapter_2/guessing_game/src/main.rs
+++ chapter_2/guessing_game/src/main.rs
@@ -2,8 +2,29 @@ extern crate rand;
 use rand::Rng;
 use std::{cmp::Ordering, io};
 
+pub struct Guess {
+    value: u8,
+}
+
+impl Guess {
+    pub fn new(value: u8) -> Result<Guess, String> {
+        if !(1..=100).contains(&value) {
+            return Err(format!(
+                "Guess value must be between 1 and 100, got {}",
+                value
+            ));
+        }
+
+        Ok(Guess { value })
+    }
+
+    pub fn value(&self) -> u8 {
+        self.value
+    }
+}
+
 fn main() {
-    println!("Guess the number!");
+    println!("Guess the number between 1 and 100!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
@@ -16,14 +37,23 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        println!("You guessed: {}", guess);
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
+        let guess = match guess.trim().parse() {
+            Ok(num) => match Guess::new(num) {
+                Ok(guess) => guess,
+                Err(e) => {
+                    println!("{}", e);
+                    continue;
+                }
+            },
+            Err(_) => {
+                println!("Please enter a valid value!");
+                continue;
+            }
         };
 
-        match guess.cmp(&secret_number) {
+        println!("You guessed: {}", guess.value());
+
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small"),
             Ordering::Greater => println!("Too big"),
             Ordering::Equal => {
