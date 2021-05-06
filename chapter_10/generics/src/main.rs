15738dcf039d9b37e5493d5f2f9caa478819d56c
salty-max <max@jellycat.fr>
2021-05-06 23:31:29 +0200

    generics pt 2

--- chapter_10/generics/src/main.rs
+++ chapter_10/generics/src/main.rs
@@ -3,6 +3,37 @@ struct Point<T> {
     y: T,
 }
 
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+
+    fn y(&self) -> &T {
+        &self.y
+    }
+}
+
+#[derive(Debug)]
+struct ComplexPoint<T, U> {
+    x: T,
+    y: U,
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+impl<T, U> ComplexPoint<T, U> {
+    fn mixup<V, W>(self, other: ComplexPoint<V, W>) -> ComplexPoint<T, W> {
+        ComplexPoint {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
@@ -14,6 +45,15 @@ fn main() {
 
     let integer = Point { x: 5, y: 10 };
     let float = Point { x: 1.0, y: 4.0 };
+
+    println!("x is {}, y is {}", integer.x(), integer.y());
+    println!("distance from origin: {}", float.distance_from_origin());
+
+    let complex1 = ComplexPoint { x: 1, y: 5.0 };
+    let complex2 = ComplexPoint { x: 'a', y: true };
+    let complex3 = complex1.mixup(complex2);
+
+    println!("FUSIOOON {:?}", complex3);
 }
 
 fn largest_int(list: &[i32]) -> &i32 {
@@ -40,14 +80,14 @@ fn largest_char(list: &[char]) -> &char {
     largest
 }
 
-fn largest<T>(list: &[T]) -> &T {
-    let mut largest = &list[0];
+// fn largest<T>(list: &[T]) -> &T {
+//     let mut largest = &list[0];
 
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
 
-    largest
-}
+//     largest
+// }
